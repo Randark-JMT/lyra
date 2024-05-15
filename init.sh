5b307f6dccf763adbd2717a13b1b4ba0c636f263
Randark <randark@foxmail.com>
2024-05-15 16:41:28 +0000

    Pending changes exported from your codespace

---
+++ init.sh
@@ -0,0 +1,8 @@
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt update && sudo apt install bazel-5.3.2
+python3 -m pip install --upgrade pip
+pip install numpy
+bazel-5.3.2 build -c opt lyra/cli_example:decoder_main
